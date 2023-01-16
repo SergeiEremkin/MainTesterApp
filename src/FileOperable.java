f4e034845d3c429d1c39aa2c263dd65c56676ca6
Sergei Eremkin <ffr@list.ru>
2023-01-17 01:05:32 +0300

    1.Буду писать сам диплом в readme.md. 2.Слелал консольный интерефейс. Реализовал функционал добавления теста через консоль

--- src/FileOperable.java
+++
@@ -1,7 +0,0 @@
-import java.io.IOException;
-
-public interface FileOperable {
-    public void writeToFile(Question question);
-    public Question readFromFile() throws IOException;
-}
-
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,14 @@
+
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Question question = new Question("Сколько стоит рыба?");
-        question.place("50", true);
-        question.place("26", false);
-        question.place("56", false);
-        Question question2 = new Question("Когда это происходит?");
-        question2.place("днём", true);
-        question2.place("вечером", false);
-        question2.place("в обед", true);
-        FileOperations operations = new FileOperations();
-        operations.writeToFile(question);
-        operations.writeToFile(question2);
-       operations.readFromFile();
+        TestController testController = new TestController();
+        testController.start();
+
+
+
 
 
 
